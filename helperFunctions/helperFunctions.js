a241ddf86fc800c0ca5c359330e0c176cbba4b33
Shubham <shubham1251997@gmail.com>
2021-12-06 22:12:30 +0530

    add helper functions to work with NJS-Listener

--- helperFunctions/helperFunctions.js
+++ helperFunctions/helperFunctions.js
@@ -34,3 +34,34 @@ let createHash = (object) => {
   let hashedObject = crypto.createHash("sha256").update(objectString);
   return hashedObject.digest("hex");
 };
+let encryptData = (object, initVector, securityKey) => {
+  let text = JSON.stringify(object);
+  const cipher = crypto.createCipheriv(algorithm, securityKey, initVector);
+  let encryptedText = cipher.update(text, "utf8", "hex");
+  //
+  encryptedText += cipher.final("hex");
+  //   console.log("Encrypted message: " + encryptedText);
+  return encryptedText;
+};
+let generateArrayOfHashedAndEncryptedObjectsFromInputArray = (arr) => {
+  let i = 0;
+  //   to be replaced by env keys later
+  let initVector = "5183666c72eec9e4";
+  let secretKey = "bf3c199c2470cb477d907b1e0917c17b";
+  let arrayOFHashedAndEncryptedObjects = [];
+  arrayOFHashedAndEncryptedObjects = arr.map((object) => {
+    let hashedObject = { ...object, secret_key: createHash(object) };
+    let encryptedObject = encryptData(hashedObject, initVector, secretKey);
+    return encryptedObject;
+  });
+  console.log("encryptedArrayis @@@@ ", arrayOFHashedAndEncryptedObjects);
+  return arrayOFHashedAndEncryptedObjects;
+};
+// decryption helper fn to work with NJS-Listener
+let decryptData = (encryptedText, initVector, securityKey) => {
+  const decipher = crypto.createDecipheriv(algorithm, securityKey, initVector);
+  let decryptedData = decipher.update(encryptedText, "hex", "utf-8");
+  decryptData += decipher.final("utf-8");
+  //   console.log("Decrypted message: " + decryptedData);
+  return JSON.parse(decryptedData);
+};
