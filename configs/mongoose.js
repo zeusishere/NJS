b7887d530355b0996fc857cd0cc3c7d3aaf29ce0
Shubham <shubham1251997@gmail.com>
2021-12-07 07:48:21 +0530

    final draft

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 // connecting to mongodb db
-mongoose.connect("<connectionString>");
+mongoose.connect(
+  "mongodb+srv://shubham:2424554@cluster0.w9vzk.mongodb.net/node_auth?retryWrites=true&w=majority"
+);
 // acquire the connection to db
 const db = mongoose.connection;
 // registering handlers on the connnection
